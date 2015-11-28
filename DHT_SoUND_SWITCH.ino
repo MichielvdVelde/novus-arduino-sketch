8ec62dbaa774694f012e3936046dcc9550acefeb
Michiel van der Velde <michiel@michielvdvelde.nl>
2015-11-29 00:25:35 +0100

    First commit

---
+++ DHT_SoUND_SWITCH.ino
@@ -0,0 +1,164 @@
+/* ----------------------------------------------------------
+
+      ARDUINO SENSOR AND 433MHZ SWITCH TRANSMITTER SKETCH
+                   VERION 0.2 - 6-7 OCT. 2013
+                  AUTHOR MICHIEL VAN DER VELDE
+                  
+-------------------------------------------------------------*/
+
+// ------------------------
+//  INCLUDES
+// ------------------------
+#include "DHT.h"
+#include <RCSwitch.h>
+
+#include <IRremote.h>
+
+// ------------------------
+//  DEFINES
+// ------------------------
+#define DHTPIN 2
+#define DHTTYPE DHT22
+
+#define ENVCHECKINTERVALMS 60000
+int DEVID = 1001;
+
+int RECV_PIN = 11;
+
+// ------------------------
+//  OBJECT CREATION
+// ------------------------
+DHT dht(DHTPIN, DHTTYPE);
+RCSwitch mySwitch = RCSwitch();
+
+IRrecv irrecv(RECV_PIN);
+decode_results results;
+byte previous_ir_code;
+
+// ------------------------
+//  GLOBAL VARS - SENSORS
+// ------------------------
+boolean envCheckStartup = true;
+long lastEnvCheckMs = 0;
+boolean soundHeard = false;
+
+float lastTemperature;
+float lastHumidity;
+
+// ------------------------
+//  GLOBAL VARS - SWITCHH
+// ------------------------
+boolean newCmd = false;
+int groupID = 0;
+int switchID = 0;
+int switchCmd = 0;
+
+// ------------------------
+//  GLOBAL VARS - LDR
+// ------------------------
+int LDR_Pin = A0;
+int lastLDRValue = 0;
+
+// ------------------------
+//  MAIN ARDUINO METHODS
+// ------------------------
+
+void setup() {
+  Serial.begin(9600);
+  
+  // Setup sensors
+  attachInterrupt(1, flipSoundHeard, RISING); 
+  dht.begin();
+  
+  // Setup switch
+  mySwitch.enableTransmit(10);
+  //Serial.println("DEVID 1001");
+  
+  // Setup IR
+  irrecv.enableIRIn();
+  
+  // ---------------------------------
+}
+
+void loop() {
+  if(envCheckStartup || (lastEnvCheckMs + ENVCHECKINTERVALMS) <= millis()) {
+    if(envCheckStartup)
+      envCheckStartup = false;
+    float newTemperature = dht.readTemperature();
+    float newHumidity = dht.readHumidity();
+    int newLDRValue = analogRead(LDR_Pin);
+    
+    if(!isnan(newTemperature) && !isnan(newHumidity)) {
+        if(lastTemperature != newTemperature) {
+          Serial.print("TMP ");
+          Serial.print(newTemperature);
+          Serial.print(" ");
+          lastTemperature = newTemperature;
+        }
+        if(lastHumidity != newHumidity) {
+          Serial.print("HUM ");
+          Serial.print(newHumidity);
+          Serial.print(" ");
+          lastHumidity = newHumidity;
+        }
+    }
+    Serial.print("LDR ");
+    Serial.println(newLDRValue);
+    
+    lastEnvCheckMs = millis();
+  }
+  
+  if(soundHeard) {
+    soundHeard = false;
+    Serial.println("SND 1");
+  }
+  
+  // IR
+  if (irrecv.decode(&results)) {
+    byte send_ir = results.value;
+    if(send_ir == 255)
+      send_ir = previous_ir_code;
+    else
+      previous_ir_code = results.value;
+    Serial.print("IR ");
+    Serial.println(send_ir, HEX);
+    irrecv.resume();
+  }
+  
+  // Run switch
+  processSerialForSwitch();
+  if(newCmd) {
+    switch(switchCmd) {
+      case 0:
+        mySwitch.switchOff(groupID, switchID);
+      break;
+      case 1:
+        mySwitch.switchOn(groupID, switchID);
+      break;
+    }
+    newCmd = false;
+  }
+}
+
+// ------------------------
+//  HELPER METHODS - SWITCH
+// ------------------------
+
+void processSerialForSwitch() {
+  if(Serial.available() == 5) {
+    Serial.read(); // discard first byte (S)
+    Serial.read(); // discard second byte (space)
+    groupID = Serial.read() - 48;
+    switchID = Serial.read() - 48;
+    switchCmd = Serial.read() - 48;
+    newCmd = true;
+  }
+}
+
+// ------------------------
+//  INTERRUPT HANDLER
+// ------------------------
+
+void flipSoundHeard() {
+  soundHeard = true;
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# novus-arduino-sketch
+
+This is the Arduino sketch I am using for my home automation system (Novus).
+
+The sketch runs on an Arduino board which has the following:
+
+* A temperature and humidity sensor ([DHT-22](https://www.sparkfun.com/products/10167))
+* A sensor that signals when loud noise is heard
+* A 433mhz transmitter
+
+This is a highly specific sketch and may not be that interesting. For more info, [read the readme for the component attached to it](https://github.com/MichielvdVelde/novus-serial).
